8f20f361f731092aa4dbde917539fed85ec9afc4
mohammedbobazine <mohammedboubazine28@gmail.com>
2023-01-18 18:05:33 +0100

    chakUser, requireAuth :valid

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -7,19 +7,35 @@ module.exports.checkUser = (req, res, next) => {
   if (token) {
     jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
       if (err) {
-        res.locals.user = nul;
+        res.locals.user = null;
         res.cookie("jwt", "", { maxAge: 1 });
         next();
       } else {
         let user = await UserModel.findById(decodedToken.id);
         //console.log({ deconctedToken: decodedToken.id });
         res.locals.user = user;
-        //console.log({ chekUser: user });
+        console.log({ chekUser: user });
         next();
       }
     });
   } else {
-    res.locals.user = nul;
+    res.locals.user = null;
     next();
+  }
+};
+
+module.exports.requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log({ decodededToken: decodedToken.id });
+        next();
+      }
+    });
+  } else {
+    console.log("No Token");
   }
 };
--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const { checkUser } = require("./middleware/auth.middleware");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("*", checkUser);
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 
 //routes
 app.use("/api/user", userRoutes);
