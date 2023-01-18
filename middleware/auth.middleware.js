28261635021ce108d5bf449bcb59b2eddb21e922
mohammedbobazine <mohammedboubazine28@gmail.com>
2023-01-18 16:03:57 +0100

    midleware : checkUser

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Models/user.model");
+
+module.exports.checkUser = (req, res, next) => {
+  const token = req.cookie.jwt;
+  if (token) {
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = nul;
+        res.cookie("jwt", "", { maxAge: 1 });
+        next();
+      } else {
+        let user = await UserModel.findById(decodedToken.id);
+        res.locals.user = user;
+        console.log(user);
+        next();
+      }
+    });
+  } else {
+    res.locals.user = nul;
+    next();
+  }
+};
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
+const { checkUser } = require("./middleware/auth.middleware");
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("*", checkUser);
 
 //routes
 app.use("/api/user", userRoutes);
